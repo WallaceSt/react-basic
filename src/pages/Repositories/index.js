68e8634c290d80056680e055fd7ff36aa31b698a
Wallace <wallacycleyton@gmail.com>
2022-07-05 19:54:38 -0300

    Utilizando dados do local storage para popular pagina

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,12 +1,22 @@
+import { useEffect, useState } from "react";
 import * as S from "./styled";
 
 export default function Repositories() {
+    const [repositories, setRepositories] = useState([]);
+    useEffect(() => { 
+        let repositoriesNames = localStorage.getItem("repositoriesNames");
+        repositoriesNames = JSON.parse(repositoriesNames);
+        setRepositories(repositoriesNames);
+        localStorage.clear();
+    }, [])
+
     return (
         <S.Container>
             <S.Title>Repositórios</S.Title>
             <S.List>
-                <S.ListItem>Repo 1</S.ListItem>
-                <S.ListItem>Repo 2</S.ListItem>
+                {
+                    repositories.map(repository => <S.ListItem>Repositório: { repository }</S.ListItem>)    
+                }
             </ S.List>
         </S.Container>
     )
