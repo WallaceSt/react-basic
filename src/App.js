ac1faedb796adb06afdcbd05481c9a60e5031690
Wallace <wallacycleyton@gmail.com>
2022-07-05 15:52:53 -0300

    Adicionando um hook state ao App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+
+
 function App(props) {
+  const [usuario, setUsuario] = useState('Wallace')
   return (
     <>
       <h1>
-        {props.title}
+        {props.title}! <br/> Meu nome é {usuario}.
       </h1>
       <input placeholder="Usuário" name="usuario" className="usuario" id="usuario" />
+      <button type='button'>Pesquisar</button>
     </>
   );
 }
