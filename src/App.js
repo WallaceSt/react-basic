6dcc3d29f278167e6b37c43623a7d4cb4d0c800a
Wallace <wallacycleyton@gmail.com>
2022-07-05 16:03:13 -0300

    Adicionando tratamento de eventos aos campos de formulário

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import { useState } from 'react';
 
 
 function App(props) {
-  const [usuario, setUsuario] = useState('Wallace')
+  const [usuario, setUsuario] = useState('WallaceSt')
+
+  function handlePesquisa() {
+    console.log(usuario)
+  }
+
   return (
+
     <>
       <h1>
         {props.title}! <br/> Meu nome é {usuario}.
       </h1>
-      <input placeholder="Usuário" name="usuario" className="usuario" id="usuario" />
-      <button type='button'>Pesquisar</button>
+      <input placeholder="Usuário" name="usuario" onChange={e => {
+        setUsuario(e.target.value);
+      }} value={usuario} />
+      <button type='button' onClick={ handlePesquisa }>Pesquisar</button>
     </>
   );
 }
