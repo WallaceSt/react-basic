53e523dd7f4fb9135b35f81a57208f0d50e20341
Wallace <wallacycleyton@gmail.com>
2022-07-05 18:03:58 -0300

    Adicionando funções axios para fazer chamada na API do Github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import { useState } from 'react';
-
+import axios from 'axios';
 
 function App(props) {
   const [usuario, setUsuario] = useState('WallaceSt')
 
   function handlePesquisa() {
-    console.log(usuario)
+    axios
+      .get(`https://api.github.com/users/${usuario}/repos`)
+      .then(response => {
+        console.log(response.data)
+      })
+      .catch(err => { 
+        console.log(err);
+      })
   }
 
   return (
-
     <>
       <h1>
         {props.title}! <br/> Meu nome é {usuario}.
