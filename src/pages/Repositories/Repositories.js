3906fa6c7ed4941bd697cffa98deb57e36b683a0
Wallace <wallacycleyton@gmail.com>
2022-07-05 18:42:27 -0300

    Organizando pastas e arquivos

---
+++ src/pages/Repositories/Repositories.js
@@ -0,0 +1,11 @@
+export default function Repositories() {
+    return (
+        <>
+            <h1>Repositórios</h1>
+            <ul>
+                <li>Repo 1</li>
+                <li>Repo 2</li>
+            </ul>
+        </>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import Repositories from "./Repositories";
+import Home from "./pages/Home/Home";
+import Repositories from "./Repositories/Repositories";
 
 export default function Router() {
     return (
